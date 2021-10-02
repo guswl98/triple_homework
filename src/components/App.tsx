9b3d32ca5da84f5c2b504c84f665e6d70fbde2b9
guswl98 <43779313+guswl98@users.noreply.github.com>
2021-10-02 18:59:05 +0900

    Add files for Tripple Logo With Award

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import './App.css';
+import TrippleAward from './TrippleAward';
 
 export default function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <TrippleAward />
+    </div>
+  );
 }
---
+++ src/components/TrippleAward.tsx
@@ -0,0 +1,5 @@
+import './TrippleAward.css';
+
+export default function TrippleAward() {
+  return <div className="award_logo">2021년 5월 기준</div>;
+}
