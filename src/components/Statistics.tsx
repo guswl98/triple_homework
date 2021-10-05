a49eda2b43157d50d95aa502902e832393477e79
guswl98 <43779313+guswl98@users.noreply.github.com>
2021-10-05 19:39:47 +0900

    Apply function for get duration according to the length of number

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -7,26 +7,46 @@ export default function Statistics() {
   const storeNum = 650;
   const duration = 2500;
 
+  const getDuration = (startNumber: number, endNumber: number): number => {
+    return (endNumber - startNumber).toString().length * 800;
+  };
+
+  const userDuration: number = getDuration(0, userNum);
+  const reviewDuration: number = getDuration(0, reviewNum);
+  const storeDuration: number = getDuration(0, storeNum);
+
   return (
     <div className="statistics_text_container fade_in">
       <div>
         <span className="bold">
-          <Counter startNumber={0} endNumber={userNum} duration={duration} />만
-          명
+          <Counter
+            startNumber={0}
+            endNumber={userNum}
+            duration={userDuration}
+          />
+          만 명
         </span>
         의 사용자
       </div>
       <div>
         <span className="bold">
-          <Counter startNumber={0} endNumber={reviewNum} duration={duration} />
+          <Counter
+            startNumber={0}
+            endNumber={reviewNum}
+            duration={reviewDuration}
+          />
           만 개
         </span>
         의 리뷰
       </div>
       <div>
         <span className="bold">
-          <Counter startNumber={0} endNumber={storeNum} duration={duration} />만
-          개
+          <Counter
+            startNumber={0}
+            endNumber={storeNum}
+            duration={storeDuration}
+          />
+          만 개
         </span>
         의 저장
       </div>
