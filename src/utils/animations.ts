1ef43edd44cec95d6630b78d4a3074e62c817e7a
guswl98 <43779313+guswl98@users.noreply.github.com>
2021-10-05 19:37:53 +0900

    Change function to return floor value

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -4,5 +4,7 @@ export const easeOutExpo = (
   c: number,
   d: number,
 ): number => {
-  return Math.ceil(t === d ? b + c : c * (-Math.pow(2, (-10 * t) / d) + 1) + b);
+  return Math.floor(
+    t === d ? b + c : c * (-Math.pow(2, (-10 * t) / d) + 1) + b,
+  );
 };
